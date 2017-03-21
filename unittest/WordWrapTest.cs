e72278f6e8c8fff2e964014ff3768537ae14f21b
Chaiwat Matarak <chaiwat.mat.dev@gmail.com>
2017-03-21 21:52:24 +0700

    migrate dotnet to version 1.0.1

--- unittest/WordWrapTest.cs
+++ unittest/WordWrapTest.cs
@@ -1,10 +1,9 @@
 using System;
-using NUnit.Framework;
+using Xunit;
 using Chaiwatmat.WordWrap;
 
 namespace Chaiwatmat.WordWrap.Test
 {
-    [TestFixture]
     public class WordWrapTest
     {
         private WordWrap _wordWrap;
@@ -13,11 +12,12 @@ namespace Chaiwatmat.WordWrap.Test
             _wordWrap = new WordWrap();
         }
 
-        [TestCase("Hello", 4, "Hell...")]
-        [TestCase("Hello", 10, "Hello")]
+        [Theory]
+        [InlineData("Hello", 4, "Hell...")]
+        [InlineData("Hello", 10, "Hello")]
         public void TestWrapString_ShouldCorrect(string text, int length, string expected){
             var result = _wordWrap.Wrap(text, length);
-            Assert.AreEqual(expected, result);
+            Assert.Equal(expected, result);
         }
     }
 }
